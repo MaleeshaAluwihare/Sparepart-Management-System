82097f5af1b55ac271a2fa9918ad2f1d40603bc5
MaleeshaAluwihare <159033744+MaleeshaAluwihare@users.noreply.github.com>
2024-02-05 22:39:29 +0530

    created delete customer servlet

---
+++ deleteCusServ.java
@@ -0,0 +1,40 @@
+package com.System;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.System.newDButil;
+
+
+@SuppressWarnings("unused")
+@WebServlet("/deleteCusServ")
+public class deleteCusServ extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String name=request.getParameter("name");
+		
+		boolean Istrue;
+		
+		Istrue = newDButil.deletecontact(name);
+		
+		if(Istrue=true) {
+			RequestDispatcher dis = request.getRequestDispatcher("new.jsp");
+			dis.forward(request, response);
+			
+		}else {
+			
+			RequestDispatcher dis2 = request.getRequestDispatcher("fail.jsp");
+			dis2.forward(request, response);
+		}
+	}
+
+}
+
